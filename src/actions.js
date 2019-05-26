ec48b3b4fd827ed005608280c5e9f51d10e7b0b9
MaciejKut <maciej.kutzmann@gmail.com>
2019-05-26 22:24:06 +0200

    Initial commit - prepared repo not working yet

---
+++ src/actions.js
@@ -0,0 +1,56 @@
+import uuid from 'uuid';
+
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+
+function addComment(text) {
+    return
+    {
+        type: ADD_COMMENT,
+            id: uuid.v4(),
+                text: text
+    }
+}
+
+
+function removeComment(id) {
+    return
+    {
+        type: REMOVE_COMMENT,
+            id
+    }
+}
+function editComment(id, text) {
+    return
+    {
+        type: EDIT_COMMENT,
+            id,
+            text
+    }
+}
+function thumbDownContent(id) {
+    return
+    {
+        type: THUMB_DOWN_COMMENT,
+            id
+    }
+}
+function thumbUpContent(id) {
+    return
+    {
+        type: THUMB_UP_COMMENT,
+            id
+    }
+}
+
+bindAddComment = text => dispatch(addComment(text));
+bindRemoveComment = text => dispatch(removeComment(id));
+bindEditComment = (text, id) => dispatch(editComment(id, text));
+bindThumbUpContent = id => dispatch(thumbUpContent(id));
+bindThumbDownContent = id => dispatch(thumbDownContent(id));
+
+export { addComment, removeComment, editComment, thumbDownContent, thumbUpContent };
---
+++ src/comments.js
@@ -0,0 +1,65 @@
+import { ADD_COMMENT, REMOVE_COMMENT, EDIT_COMMENT, THUMB_UP_COMMENT, THUMB_DOWN_COMMENT } from './actions';
+
+function comments(state = [], action) {
+    switch (action.type) {
+        case ADD_COMMENT:
+            return [
+                {
+                    id: action.id,
+                    text: action.text,
+                    votes: 0
+                }
+                , ...state]
+
+        case REMOVE_COMMENT:
+            return [
+                state.comments.filter(comment => comment.id !== action.id)
+            ]
+        case EDIT_COMMENT:
+            return [
+                state.comments.map(comment => {
+                    if (comment.id !== action.id) {
+                        return comments
+                    }
+
+                    return Object.assign({}, comment, {
+                        text: action.text
+                    })
+                })
+            ]
+        case THUMB_DOWN_COMMENT:
+            return [
+                state.comments.map(comment => {
+                    if (comment.id !== action.id) {
+                        return comments
+                    }
+
+                    return Object.assign({}, comment, {
+                        votes: state.votes - 1
+                    })
+                })
+            ]
+        case THUMB_UP_COMMENT:
+            return [
+                state.comments.map(comment => {
+                    if (comment.id !== action.id) {
+                        return comments
+                    }
+
+                    return Object.assign({}, comment, {
+                        votes: state.votes + 1
+                    })
+                })
+            ]
+        default:
+            return state;
+    }
+}
+
+
+const initialState = {
+    comments: [],
+    users: []
+}
+
+export default comments;
