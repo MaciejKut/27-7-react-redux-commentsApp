fab6c06d691083104e3eb28be645c2995c327ab7
MaciejKut <maciej.kutzmann@gmail.com>
2019-05-28 00:02:02 +0200

    Add DevTools

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import reducer from './reducer';
 import { addComment } from './actions';
-import { createLogger } from 'redux-logger';
+import DevTools from './DevTools';
 
-const logger = createLogger();
+
 const store = createStore(reducer,
-    applyMiddleware(logger));
+    DevTools.instrument());
 ReactDOM.render(
     <Provider store={store}>
         <App />
